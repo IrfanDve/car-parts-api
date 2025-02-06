622dc11121c8d48cb0164c31996c3621e1ed9448
irfanullah <irfanullah.umbs@gmail.com>
2025-02-07 00:53:31 +0500

    Sanctum user authentication is completed

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -15,19 +15,12 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'Validation failed',
-                'errors' => $validator->errors(),
-            ], 422);
-        }
-        
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -44,17 +37,10 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
-    
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'Validation failed',
-                'errors' => $validator->errors(),
-            ], 422);
-        }
 
         $user = User::where('email', $request->email)->first();
 
